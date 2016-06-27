9f6e6f07bdace92831df949a8d92b1e333c6eba1
modle <odle.matthew@gmail.com>
2016-06-26 23:28:49 -0500

    added test as a build dependency

--- src/test/java/com/kosn/util/WorldTest.java
+++ src/test/java/com/kosn/util/WorldTest.java
@@ -8,10 +8,4 @@ public class WorldTest {
 	public void helloWorldTestShouldPass() {
 		assert "DERP".equals("DERP");
 	}
-
-	@Test(description="Hello World TestNG Failing" )
-	public void helloWorldTestShouldFail() {
-		System.out.println("should fail");
-		assert "DERP".equals("asdf");
-	}
 }
